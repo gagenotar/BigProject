db008364f12d605acd3ae544e2a679f4cd08c207
Gage Notargiacomo <gagenotar@gmail.com>
2024-06-25 18:22:53 -0400

    React Setup

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './Header.css';
+
+const Header = () => {
+
+    return (
+        <>
+            <div id="test-div">a div</div>
+            <header>
+                <h1>My Site</h1>
+                <button type="button" class="btn btn-primary">Primary</button>
+            </header>
+        </>
+    );
+};
+
+export default Header;
