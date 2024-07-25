7eea78b9784432c4e54c7ca8c1e745e071910909
Gage Notargiacomo <gagenotar@gmail.com>
2024-07-25 13:47:39 -0400

    App routes
    Added testing routes that do not enforce JWT

---
+++ routes/entryAppRoutes.js
@@ -0,0 +1,34 @@
+// routes/entryRoutes.js
+const express = require('express');
+const router = express.Router();
+const entryController = require('../controllers/entryController.js');
+const upload = require('../middleware/upload.js');
+
+router.route('/addEntry')
+  .post(upload.single('image'), entryController.addEntry);
+
+router.route('/createEntry')
+  .post(entryController.createEntry);
+
+router.route('/deleteEntry/:id')
+  .delete(entryController.deleteEntryByID);
+
+router.route('/editEntry/:id')
+  .put(upload.single('image'), entryController.editEntryByID);
+
+router.route('/getEntry/:id')
+  .get(entryController.getEntryByID);
+
+router.route('/searchEntries')
+  .post(entryController.searchEntries);
+
+router.route('/searchMyEntries')
+  .post(entryController.searchMyEntries);
+
+router.route('/profile/:id')
+  .get(entryController.profileByID);
+
+router.route('/updateProfile/:id')
+  .put(entryController.updateProfileByID);
+
+module.exports = router;
