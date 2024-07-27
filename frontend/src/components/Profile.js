b61db0090f23f6d9f0c6cf55ba1f23fa1930ab8e
Dylan Moo Hernandez <dylangmooh@gmail.com>
2024-07-26 21:28:48 -0400

    Profile page UI update

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -179,32 +179,39 @@ const Profile = () => {
   return (
     <div className="profile-container">
       <div className="profile-card">
-        <h2>Profile</h2>
+        <h3 className='profile-title'>Profile Details</h3>
         <p><strong>First Name:</strong> {profile.firstName}</p>
         <p><strong>Last Name:</strong> {profile.lastName}</p>
         <p><strong>Email:</strong> {profile.email}</p>
         <p><strong>Login:</strong> {profile.login}</p>
         <p><strong>Password:</strong> ******</p>
         
+        <h3 className='update-prompt'>Update Profile</h3>
         <form className="profile-form" onSubmit={handleUpdateProfile}>
-          <h3>Update Profile</h3>
-          <div>
-            <label>New Login:</label>
-            <input
-              type="text"
-              value={newLogin}
-              onChange={(e) => setNewLogin(e.target.value)}
-            />
+          <div className="form-floating mb-3">
+              <input 
+                  type="text" 
+                  className="form-control form-control-sm" 
+                  id="floatingInput" 
+                  placeholder="username"
+                  value={newLogin}
+                  onChange={(e) => setNewLogin(e.target.value)} 
+              />
+              <label htmlFor="floatingInput">New Login:</label>
           </div>
-          <div>
-            <label>New Password:</label>
-            <input
-              type="password"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-            />
+
+          <div className="form-floating mb-3">
+              <input 
+                  type="password" 
+                  className="form-control form-control-sm" 
+                  id="floatingInput" 
+                  placeholder="Password"
+                  value={newPassword}
+                  onChange={(e) => setNewPassword(e.target.value)} 
+              />
+              <label htmlFor="floatingInput">New Password:</label>
           </div>
-          <button type="submit">Update Profile</button>
+          <button type="submit" className='btn btn-primary w-100' id='update-btn'>Update Profile</button>
         </form>
         {message && <p className="message">{message}</p>}
       </div>
