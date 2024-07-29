fbd4b9a5d633b41627b39d61f44665cef1d5431e
Gage Notargiacomo <125597868+gagenotar@users.noreply.github.com>
2024-07-29 17:30:13 +0000

    Initial file upload

--- controllers/authController.js
+++ controllers/authController.js
@@ -125,8 +125,6 @@ exports.register = async (req, res) => {
   }
 };
 
-
-
 exports.verifyCode = async (req, res) => {
   const { email, code } = req.body;
 
@@ -150,6 +148,51 @@ exports.verifyCode = async (req, res) => {
   } catch (e) {
     res.status(500).json({ error: e.toString() });
   }
+}
+
+exports.forgotPassword = async (req, res) => {
+  const { email } = req.body;
+
+  try {
+    const db = client.db('journeyJournal');
+    const user = await db.collection('user').findOne({ email });
+    if (!user) {
+      return res.status(400).json({ error: 'User not found' });
+    }
+
+    const resetCode = Math.floor(100000 + Math.random() * 900000).toString(); // Generate a 6-digit code
+    await db.collection('user').updateOne(
+      { email },
+      { $set: { resetPasswordCode: resetCode, resetPasswordExpires: Date.now() + 3600000 } } // Code expires in 1 hour
+    );
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: 'JourneyJournal Password Reset Code',
+      text: `You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n
+        Your password reset code is: ${resetCode}\n\n
+        If you did not request this, please ignore this email and your password will remain unchanged.\n`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.error('Error sending email:', error);
+
+        return res.status(500).json({ error: 'Error sending email', details: error.toString() });
+      } else {
+        console.log('Password reset email sent:', info.response);
+        res.status(200).json({ message: 'Password reset code sent to email' });
+      }
+    });
+  } catch (e) {
+    console.error('Error in forgot-password endpoint:', e);
+    res.status(500).json({ error: e.toString() });
+  }
+}
+
+exports.resetPassword = async (req, res) => {
+
 }
 
 exports.refreshToken = (req, res) => {
